f4cb087849e19e2e588a9b1fabdb83b59240ab56
Moritz Petersen <moritz.petersen@freenet.de>
2016-04-22 13:16:16 +0200

    Fix segfault and default to PWD
    - if no path is given, compress $PWD
    - if path is $HOME, don't try to compress

--- pwdxz.c
+++ pwdxz.c
@@ -5,13 +5,9 @@
 
 int main(int argc, char** argv)
 {
-	if (argc < 2) {
-		fprintf(stderr, "Error: No directory specified\n");
-		return 1;
-	}
 	char *home = getenv("HOME");
 	size_t l_home = strlen(home);
-	char *cwd = argv[1];
+	char *cwd = argc > 1 ? argv[1] : getenv("PWD");
 	// is cwd a subdirectory of home ?
 	bool sub_home = memcmp(home, cwd, l_home) == 0;
 	// number of directories below root/home
@@ -25,6 +21,11 @@ int main(int argc, char** argv)
 			i_basename = i;
 			++depth;
 		}
+	// print prefix
+	if (sub_home)
+		printf("~");
+	if (depth == 0)
+		return 0;
 	char *short_dirs = malloc(sizeof(char) * (2 * (depth - 1) + 1));
 	for (char *i = i_midpath, *j = short_dirs; i < i_basename; i++)
 		if (*i == '/') {
@@ -32,9 +33,6 @@ int main(int argc, char** argv)
 			*(j++) = *(++i);
 		}
 	*(short_dirs + 2 * (depth - 1)) = '\0';
-	// print prefix
-	if (sub_home)
-		printf("~");
 	printf("%s%s\n", short_dirs, i_basename);
 	free(short_dirs);
 }
